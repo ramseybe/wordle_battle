a63aab554685e41e0c865c0e2e68b52fbcf0b795
ramseybe <79168952+ramseybe@users.noreply.github.com>
2023-04-05 14:45:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import streamlit.components.v1 as components
 df_ben=pd.read_csv("ben_wordle.csv")
 df_juuls=pd.read_csv("juuls_wordle.csv")
 df=pd.merge(df_ben, df_juuls, on=["date","word"])
@@ -8,6 +9,24 @@ st.dataframe(df)
 date=st.date_input("Enter Date:")
 st.write(date)
 
+
+a="""<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+
+<i class="fa fa-car"></i>
+<i class="fa fa-car" style="font-size:48px;"></i>
+<i class="fa fa-car" style="font-size:60px;color:red;"></i>
+
+</body>
+</html>"""
+
+# st.write(HTML(a))
+
+components.html(a,width=300, height=700)
 
 
 option = st.selectbox("Name?",('Ben', 'Juuls'))
