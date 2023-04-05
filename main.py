4b1276fa58129b8ff86915cb52aa29f69bf599ce
ramseybe <79168952+ramseybe@users.noreply.github.com>
2023-04-04 23:52:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,15 @@ df=pd.read_csv("wordle.csv")
 st.dataframe(df)
 date=st.date_input("Enter Date:")
 st.write(date)
+
+option = st.selectbox(
+    ('Ben', 'Juuls'))
+
+st.write('You selected:', option)
+
+
+
+if option == "Juuls":
+  pass
+elif option == "Ben":
+  pass
